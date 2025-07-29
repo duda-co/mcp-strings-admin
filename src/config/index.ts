232258fc9469e96672ac43768b7dda92973a7d35
or-givati <or.givati@duda.co>
2025-07-29 15:48:34 +0300

    change host

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ export interface Config {
 }
 
 export const config: Config = {
-  stringsAdminHost: process.env.STRINGS_ADMIN_HOST || 'http://172.31.45.202',
+  stringsAdminHost: process.env.STRINGS_ADMIN_HOST || 'http://prd-ms-strings-admin-1.dudamobile.com',
   basePath: process.env.BASE_PATH || '/ms/strings-admin/internal/',
   logLevel: (process.env.LOG_LEVEL as Config['logLevel']) || 'info',
   httpTimeout: parseInt(process.env.HTTP_TIMEOUT_MS || '5000', 10)
